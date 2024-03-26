6f603af5be44cc3475e0600d777cdbec89c005ff
Viv B <viv@place.technology>
2024-03-26 15:57:53 +1100

    feat(subnet): deploy custom vnet with much smaller subnet range
    use the smaller subnet for the clusters node pool - add resource group - name resources by resource group & environment instead of random

--- azure/main.tf
+++ azure/main.tf
@@ -1,9 +1,41 @@
-resource "random_id" "unique" {
-  byte_length = 4
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = "vnet-${var.resource_group_name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.224.0.0/12"]
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${var.resource_group_name}-${var.environment}"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes     = ["10.224.0.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+
+  tags = {
+    Environment = var.environment
+  }
 }
 
 resource "azurerm_kubernetes_cluster" "placeos" {
-    name                = "aks-placeos--${random_id.unique.hex}"
+    name                = "aks-placeos--${var.resource_group_name}-${var.environment}"
     location            = var.location
     resource_group_name = var.resource_group_name
     dns_prefix          = var.dns_prefix
@@ -13,6 +45,7 @@ resource "azurerm_kubernetes_cluster" "placeos" {
         name            = "agentpool"
         node_count      = 3
         vm_size         = var.environment == "Production" ? "Standard_DS11_v2" : "Standard_D2_v3"
+        vnet_subnet_id  = azurerm_subnet.subnet.name
     }
 
     identity {
--- azure/terraform.tfvars
+++ azure/terraform.tfvars
@@ -1,4 +1,6 @@
 location = "australiaeast"
+resource_group_name="client-name"
+environment="Development" // Use "Production" for nodes with capacity to run production workloads otherwise the name the client will use is best. eg. "Development", "UAT", "Staging"
+
 
 dns_prefix="placeos"
-environment="Development"
